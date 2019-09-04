78c991bab07692aa3e00c895e9479478cd16ff09
Leonardo Almeida <l.sales.almeida@accenture.com>
2019-09-04 18:14:10 -0300

    Criando a estrutura do projeto

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div>
+      <p>Bem-vindo a sua nova app!</p>
+    </div>
+  );
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { 
+              minimize: true 
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+};
